668dc68f93c657a7e6c013c331817a7b1d5b34b7
Fernando <fb@gamekaiju.monster>
2022-08-14 01:16:53 +0100

    Adds two gifs more.

--- src/rewardGifs.ts
+++ src/rewardGifs.ts
@@ -1,6 +1,8 @@
 const rewardGifs: string[] = [
   'https://raw.githubusercontent.com/gamekaiju/action-reward-pr/main/assets/01.gif',
   'https://raw.githubusercontent.com/gamekaiju/action-reward-pr/main/assets/02.gif',
+  'https://raw.githubusercontent.com/gamekaiju/action-reward-pr/main/assets/03.gif',
+  'https://raw.githubusercontent.com/gamekaiju/action-reward-pr/main/assets/04.gif',
 ]
 
 export default rewardGifs
